d11c4518844f8c657e55b60dbf2e5f78da94890d
Zijian Xu <xl05011728@gmail.com>
2024-03-23 11:59:31 +0000

    Define Flask routes for each of HTML pages and store the data into data.txt

--- .venv/app.py
+++ .venv/app.py
@@ -1,1 +1,32 @@
-from flask import Flask, render_template, redierect, url_for, request
+# import modules from flask and form template from forms.py
+from flask import Flask, render_template, redirect, url_for, request
+from forms import DataCollectionForm
+
+# initialize the Flask application
+app = Flask(__name__)
+# create a secret key for the application
+app.config['SECRET_KEY'] = '123'
+
+@app.route('/')
+def welcome_page():
+    return render_template('WelcomePage.html')
+
+@app.route('/information')
+def information_page():
+    return render_template('InformationPage.html')
+
+@app.route('/data_collection', methods=['GET', 'POST'])
+def data_collection_page():
+    form = DataCollectionForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            with open('data.txt', 'a') as f:
+                f.write(f"Name: {form.student_name.data}, Student Number: {form.student_number.data}, Email: {form.email.data}, Grades: {form.grades.data}, Satisfaction: {form.satisfaction.data}, Facilities: {form.facilities.data}, Feedback: {form.feedback.data}\n")
+
+            return redirect(url_for('welcome_page'))
+
+    return render_template('DataCollectionPage.html', form=form)
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
